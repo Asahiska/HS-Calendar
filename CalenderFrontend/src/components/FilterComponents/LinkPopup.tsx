bf6d3d80a8f71e5ff208ea19ceb7247abf2713ec
Felixfailw <98382575+Felixfailw@users.noreply.github.com>
2025-01-20 18:38:32 +0100

    feat:new UI functions

--- CalenderFrontend/src/components/FilterComponents/LinkPopup.tsx
+++ CalenderFrontend/src/components/FilterComponents/LinkPopup.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import { CalendarIcon, Copy, X } from "lucide-react"
+import { CalendarIcon, Copy } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 
